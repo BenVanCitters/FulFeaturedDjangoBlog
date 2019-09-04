d6968247ec1e0e97bdc834302a79bdb20a3f3db7
Ben V <van.citters.b@gmail.com>
2019-09-04 00:21:56 -0500

    completed tutorial 10

--- django_ffProject/blog/views.py
+++ django_ffProject/blog/views.py
@@ -1,9 +1,65 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request,'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] #affects list view ordering
+
+#building new with default setup
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    #valid override
+    def form_valid(self, form):
+        #set the author
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    #UserPassesTestMixin override
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    #UserPassesTestMixin override
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+
+    #valid override
+    def form_valid(self, form):
+        #set the author
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request,'blog/about.html', {'title': 'about page title'})
